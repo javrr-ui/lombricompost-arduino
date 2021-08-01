a68a5932f3fcf1dc3b9f19df8442d1fafa2835a1
Javier <framirez76@uabc.edu.mx>
2021-08-01 14:52:43 -0700

    moved fan control code to its own function, added fan control based on temperature

--- arduino-lombricompost.ino
+++ arduino-lombricompost.ino
@@ -43,21 +43,40 @@ void loop()
   DS18B20sensor.requestTemperatures();
   tempSensorC(0,"tempSensor1");
   //delay(500);
+  temperatureControl();
+}
 
+void temperatureControl(){
+
+  //fan control from serial communication
   if(Serial.available()>0){
     fanState = Serial.readString();
     if(fanState == "fanOn"){
+      fanIsOn = true;
       digitalWrite(fan,HIGH);
     }
     if(fanState == "fanOff"){
+      fanIsOn = false;
       digitalWrite(fan,LOW);
     }
   }
 
+  //fan control based on temperature
+  if(fanIsOn == false){
+    int temperature = round(DS18B20sensor.getTempCByIndex(0));
+    if(temperature > 30){
+      digitalWrite(fan,HIGH);
+    }else{
+      digitalWrite(fan,LOW);
+    }
+  }
 }
 
+
+
 void tempSensorC(int sensorId,String text){
-  String msg = ""+String(DS18B20sensor.getTempCByIndex(sensorId),2)+text;
+  float temperature = DS18B20sensor.getTempCByIndex(sensorId);
+  String msg = ""+String(temperature,2)+text;
   Serial.println(msg);
 }
 void sensorReading(int sensor,int min, int max, String texto){
